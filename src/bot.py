2ad31783b93d784647b1435a20b24e76c18cb6c9
caleb-stark <stark.caleba@gmail.com>
2026-03-10 12:30:11 -0700

    switched over to gosling utils base

--- src/bot.py
+++ src/bot.py
@@ -1,89 +1,151 @@
-from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
-from rlbot.messages.flat.QuickChatSelection import QuickChatSelection
-from rlbot.utils.structures.game_data_struct import GameTickPacket
+from tools import  *
+from objects import *
+from routines import *
 
-from util.ball_prediction_analysis import find_slice_at_time
-from util.boost_pad_tracker import BoostPadTracker
-from util.drive import steer_toward_target
-from util.sequence import Sequence, ControlStep
-from util.vec import Vec3
+# ExampleBot2 is a more complex example to showcase how to put routines to use!
 
+class ExampleBot(GoslingAgent):
+    def run(agent):
+        # This is just some code to warn us if we're offsides
+        ball_dist_to_owngoal = (agent.ball.location - agent.friend_goal.location).magnitude()
+        my_dist_to_owngoal = (agent.me.location - agent.friend_goal.location).magnitude()
+        ball_closer_than_me = ball_dist_to_owngoal < my_dist_to_owngoal
 
-class MyBot(BaseAgent):
+        left_test_a, left_test_b = Vector3(-4100 * side(agent.team), agent.ball.location.y, 0), Vector3(4100 * side(agent.team), agent.ball.location.y, 0)
+        agent.line(agent.me.location, left_test_a, (0, 255, 0))
+        agent.line(agent.me.location, left_test_b, (255, 0, 0))
 
-    def __init__(self, name, team, index):
-        super().__init__(name, team, index)
-        self.active_sequence: Sequence = None
-        self.boost_pad_tracker = BoostPadTracker()
+        # If the stack currently has routines on it, we won't do anything else.
+        # This first statement makes sure the stack is empty before evaluating what to do next
+        # Maybe in the future, you could have it look for a better shot to take even as the current one is running???
+        if len(agent.stack) < 1:
 
-    def initialize_agent(self):
-        # Set up information about the boost pads now that the game is active and the info is available
-        self.boost_pad_tracker.initialize_boosts(self.get_field_info())
+            # We want to run the kickoff routine if there's currently a kickoff.
+            # The routine isn't very smart and doesn't consider if you have any teammates, so that would be something
+            # to improve upon
+            if agent.kickoff_flag:
+                agent.push(kickoff())
+            else:
+                # If we're in match play, the first thing we do is create a dictionary of targets
+                # I will make a few. One for the opponent's goal, and one for a clear (aiming between either side of the field
+                targets = {
+                    "goal": (agent.foe_goal.left_post, agent.foe_goal.right_post),
+                    "clear": (Vector3(-4100 * side(agent.team), agent.ball.location.y, 0), Vector3(4100 * side(agent.team), agent.ball.location.y, 0))
+                }
 
-    def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
-        """
-        This function will be called by the framework many times per second. This is where you can
-        see the motion of the ball, etc. and return controls to drive your car.
-        """
+                # Once we have the dictionary complete, we pass it to the find_hits tool that looks into the future
+                # to find potential shots to take
+                shots = find_hits(agent, targets)
 
-        # Keep our boost pad info updated with which pads are currently active
-        self.boost_pad_tracker.update_boost_status(packet)
+                # Time to push a routine. Let's see if we have any shots we can take
 
-        # This is good to keep at the beginning of get_output. It will allow you to continue
-        # any sequences that you may have started during a previous call to get_output.
-        if self.active_sequence is not None and not self.active_sequence.done:
-            controls = self.active_sequence.tick(packet)
-            if controls is not None:
-                return controls
+                if len(shots["goal"]) > 0:
+                    # We do, but is it any good? I want to "score" how good each shot is by its average speed vs how
+                    # aligned we are for it!
+                    def score_shot(x):
+                        distance = (x.ball_location - agent.me.location).magnitude()
+                        speed = distance / (x.intercept_time - agent.time)
+                        return speed * x.ratio
 
-        # Gather some information about our car and the ball
-        my_car = packet.game_cars[self.index]
-        car_location = Vec3(my_car.physics.location)
-        car_velocity = Vec3(my_car.physics.velocity)
-        ball_location = Vec3(packet.game_ball.physics.location)
+                    best_shot = shots["goal"][0]
+                    best_shot_score = score_shot(shots["goal"][0])
+                    for shot in shots["goal"]:
+                        score = score_shot(shot)
+                        if score > best_shot_score:
+                            best_shot = shot
+                            best_shot_score = score
 
-        # By default we will chase the ball, but target_location can be changed later
-        target_location = ball_location
+                    # So now we've found the "best" shot, but could we do better?
+                    # It may be better to drive the car into a good position before taking a shot
+                    if score_shot(best_shot) > 500:
+                        agent.push(best_shot)
+                    elif len(shots["clear"]) > 0:
+                        agent.push(shots["clear"][0])
 
-        if car_location.dist(ball_location) > 1500:
-            # We're far away from the ball, let's try to lead it a little bit
-            ball_prediction = self.get_ball_prediction_struct()  # This can predict bounces, etc
-            ball_in_future = find_slice_at_time(ball_prediction, packet.game_info.seconds_elapsed + 2)
+                elif len(shots["clear"]) > 0:
+                    agent.push(shots["clear"][0])
 
-            # ball_in_future might be None if we don't have an adequate ball prediction right now, like during
-            # replays, so check it to avoid errors.
-            if ball_in_future is not None:
-                target_location = Vec3(ball_in_future.physics.location)
-                self.renderer.draw_line_3d(ball_location, target_location, self.renderer.cyan())
+                # If there aren't any shots to take, we need to do something else.
+                # Usually we can't take a shot because we don't have enough boost, so we'll look for a good boostpad
+                # to grab.
+                elif agent.me.boost < 30:
+                    # These variables will keep track of the best boost we find to pick up
+                    best_boost = None
+                    best_boost_value = -1.0
 
-        # Draw some things to help understand what the bot is thinking
-        self.renderer.draw_line_3d(car_location, target_location, self.renderer.white())
-        self.renderer.draw_string_3d(car_location, 1, 1, f'Speed: {car_velocity.length():.1f}', self.renderer.white())
-        self.renderer.draw_rect_3d(target_location, 8, 8, True, self.renderer.cyan(), centered=True)
+                    # I want to find a boost that is between us and the goal. We'll determine this by making a vector
+                    # pointing from us to each boost, and each boost to the goal, and see which is most similar
+                    for boost in agent.boosts:
+                        # We will ignore small or inactive boosts
+                        if not boost.large: continue
+                        if not boost.active: continue
 
-        if 750 < car_velocity.length() < 800:
-            # We'll do a front flip if the car is moving at a certain speed.
-            return self.begin_front_flip(packet)
+                        # vector from us to boost
+                        me_to_boost = (boost.location - agent.me.location).normalize()
+                        # vector from boost to our goal
+                        boost_to_goal = (agent.friend_goal.location - boost.location).normalize()
 
-        controls = SimpleControllerState()
-        controls.steer = steer_toward_target(my_car, target_location)
-        controls.throttle = 1.0
-        # You can set more controls if you want, like controls.boost.
+                        # Here's where we actually check to see if the current boost is better than the best so far.
+                        # If it's more aligned , it becomes the new "best boost"
+                        if boost_to_goal.dot(me_to_boost) > best_boost_value:
+                            best_boost_value = boost_to_goal.dot(me_to_boost)
+                            best_boost = boost
+                    if best_boost is not None:
+                        # Drive towards the best boost, but try to circle towards it so that we can go straight to our
+                        # goal afterwards
+                        agent.push(goto_boost(best_boost, agent.friend_goal.location))
 
-        return controls
+                elif ball_closer_than_me and abs(agent.ball.location.x) < 2000:
+                    # Okay, so if this code runs it means we don't have shots but we do have boost. The most likely
+                    # Case is that we're offsides. We could make a new set of targets to clear the ball, but often
+                    # a short_shot is good enough
+                    agent.push(short_shot(agent.foe_goal.location))
 
-    def begin_front_flip(self, packet):
-        # Send some quickchat just for fun
-        self.send_quick_chat(team_only=False, quick_chat=QuickChatSelection.Information_IGotIt)
+                # if we still haven't found a routine, we should probably retreat towards our own goal.
+                # Let's be smart though, heading towards the center of our goal might hit the ball into it.
+                # Instead, we'll drive towards the nearest goalpost, and collect any small boost pads along the way
+                if len(agent.stack) < 1:
+                    left_dist = (agent.friend_goal.left_post - agent.me.location).magnitude()
+                    right_dist = (agent.friend_goal.right_post - agent.me.location).magnitude()
+                    if left_dist < right_dist:
+                        target = agent.friend_goal.left_post
+                    else:
+                        target = agent.friend_goal.right_post
 
-        # Do a front flip. We will be committed to this for a few seconds and the bot will ignore other
-        # logic during that time because we are setting the active_sequence.
-        self.active_sequence = Sequence([
-            ControlStep(duration=0.05, controls=SimpleControllerState(jump=True)),
-            ControlStep(duration=0.05, controls=SimpleControllerState(jump=False)),
-            ControlStep(duration=0.2, controls=SimpleControllerState(jump=True, pitch=-1)),
-            ControlStep(duration=0.8, controls=SimpleControllerState()),
-        ])
+                    # I think it would be dumb to drive all the way back, and give the opponent a free ball
+                    # so let's move the final target somwehere between the ball and the goal.
 
-        # Return the controls associated with the beginning of the sequence so we can start right away.
-        return self.active_sequence.tick(packet)
+                    target_to_car = (agent.me.location - target).normalize()
+                    target_to_ball = (agent.ball.location - target)
+                    final_dist = target_to_car.dot(target_to_ball) * 0.5
+
+                    # This moves our target point towards our car by 1/2 the distance of the target to the ball
+                    # post-------car---------------------ball
+                    target += target_to_car * final_dist
+
+                    # This is a repeat of the earlier boost code, but with changes to work with small pads
+                    # and restrict us from going backwards to get to a pad
+                    best_boost = None
+                    best_boost_value = -1.0
+
+                    for boost in agent.boosts:
+                        if not boost.active: continue
+                        me_to_boost = (boost.location - agent.me.location).normalize()
+                        boost_to_target = (target - boost.location).normalize()
+                        if boost_to_target.dot(me_to_boost) > best_boost_value:
+                            best_boost_value = boost_to_target.dot(me_to_boost)
+                            best_boost = boost
+
+                    if best_boost is not None and best_boost_value > 0.7 and agent.me.boost < 100:
+                        agent.push(goto_boost(best_boost, target))
+                    else:
+                        # We couldn't find any boost pads to grab, so we just drive back slowly
+                        relative_target = target - agent.me.location
+                        distance = relative_target.magnitude()
+                        local_target = agent.me.local(relative_target)
+                        defaultPD(agent, local_target)
+                        # this will drive at full speed towards the target, but slow down as we get closer
+                        defaultThrottle(agent, cap(distance * 2, 0, 2300))
+
+                        # gotta make sure it doesn't use boost in this case!
+                        agent.controller.boost = False
